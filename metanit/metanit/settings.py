0dbdded6f4929a2b1d58cb1f5749ba13fcc2ac7e
Aleksandra Kim <kimaleksa5@gmail.com>
2024-11-17 13:01:47 +0300

    Добавила requirements.txt

--- metanit/metanit/settings.py
+++ metanit/metanit/settings.py
@@ -85,9 +85,13 @@ DATABASES = {
         'HOST': 'localhost',  # Replace with your PostgreSQL server's address if necessary
         'PORT': '5432',  # Leave empty to use the default PostgreSQL port (usually 5432)
         }
-
 }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
